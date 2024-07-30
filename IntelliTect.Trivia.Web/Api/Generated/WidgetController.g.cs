3350729069d7a68d2e614566350521b6777ae2e9
Meghan <Meg.Woodford@IntelliTect.com>
2024-07-30 11:38:25 -0700

    First video code

---
+++ IntelliTect.Trivia.Web/Api/Generated/WidgetController.g.cs
@@ -0,0 +1,83 @@
+
+using IntelliTect.Trivia.Web.Models;
+using IntelliTect.Coalesce;
+using IntelliTect.Coalesce.Api;
+using IntelliTect.Coalesce.Api.Behaviors;
+using IntelliTect.Coalesce.Api.Controllers;
+using IntelliTect.Coalesce.Api.DataSources;
+using IntelliTect.Coalesce.Mapping;
+using IntelliTect.Coalesce.Mapping.IncludeTrees;
+using IntelliTect.Coalesce.Models;
+using IntelliTect.Coalesce.TypeDefinition;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace IntelliTect.Trivia.Web.Api
+{
+    [Route("api/Widget")]
+    [Authorize]
+    [ServiceFilter(typeof(IApiActionFilter))]
+    public partial class WidgetController
+        : BaseApiController<IntelliTect.Trivia.Data.Models.Widget, WidgetDtoGen, IntelliTect.Trivia.Data.AppDbContext>
+    {
+        public WidgetController(CrudContext<IntelliTect.Trivia.Data.AppDbContext> context) : base(context)
+        {
+            GeneratedForClassViewModel = context.ReflectionRepository.GetClassViewModel<IntelliTect.Trivia.Data.Models.Widget>();
+        }
+
+        [HttpGet("get/{id}")]
+        [Authorize]
+        public virtual Task<ItemResult<WidgetDtoGen>> Get(
+            int id,
+            DataSourceParameters parameters,
+            IDataSource<IntelliTect.Trivia.Data.Models.Widget> dataSource)
+            => GetImplementation(id, parameters, dataSource);
+
+        [HttpGet("list")]
+        [Authorize]
+        public virtual Task<ListResult<WidgetDtoGen>> List(
+            ListParameters parameters,
+            IDataSource<IntelliTect.Trivia.Data.Models.Widget> dataSource)
+            => ListImplementation(parameters, dataSource);
+
+        [HttpGet("count")]
+        [Authorize]
+        public virtual Task<ItemResult<int>> Count(
+            FilterParameters parameters,
+            IDataSource<IntelliTect.Trivia.Data.Models.Widget> dataSource)
+            => CountImplementation(parameters, dataSource);
+
+        [HttpPost("save")]
+        [Authorize]
+        public virtual Task<ItemResult<WidgetDtoGen>> Save(
+            [FromForm] WidgetDtoGen dto,
+            [FromQuery] DataSourceParameters parameters,
+            IDataSource<IntelliTect.Trivia.Data.Models.Widget> dataSource,
+            IBehaviors<IntelliTect.Trivia.Data.Models.Widget> behaviors)
+            => SaveImplementation(dto, parameters, dataSource, behaviors);
+
+        [HttpPost("bulkSave")]
+        [Authorize]
+        public virtual Task<ItemResult<WidgetDtoGen>> BulkSave(
+            [FromBody] BulkSaveRequest dto,
+            [FromQuery] DataSourceParameters parameters,
+            [FromServices] IDataSourceFactory dataSourceFactory,
+            [FromServices] IBehaviorsFactory behaviorsFactory)
+            => BulkSaveImplementation(dto, parameters, dataSourceFactory, behaviorsFactory);
+
+        [HttpPost("delete/{id}")]
+        [Authorize]
+        public virtual Task<ItemResult<WidgetDtoGen>> Delete(
+            int id,
+            IBehaviors<IntelliTect.Trivia.Data.Models.Widget> behaviors,
+            IDataSource<IntelliTect.Trivia.Data.Models.Widget> dataSource)
+            => DeleteImplementation(id, new DataSourceParameters(), dataSource, behaviors);
+    }
+}
